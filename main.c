58ef65e9f06ccf919a75eb1b5729eb2ba2304c88
faiithal <johndumlao2355@gmail.com>
2026-05-10 12:16:30 +0800

    initial project structure

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+struct Apparel {
+    char name[30];
+    int avaiable;
+}
+
+struct Outfit {
+    struct Apparel top;
+    struct Apparel bottom;
+    struct Apparel shoes;
+    struct Apparel headwear;
+    struct Apparel accessory;
+    struct Apparel bag;
+}
+
+// TODO: Implement Check Closet
+
+// TODO: Impelement Check Outfit
+// TODO: Implement Display OOTD
+// TODO: Implement Wash Clothes
+
+int main() {
+    struct Apparel top[9];
+    struct Apparel bottom[9];
+    struct Apparel shoes[9];
+    struct Apparel headwear[9];
+    struct Apparel accessory[9];
+    struct Apparel bag[9];
+
+    // TODO: Implement Action Menu
+}
